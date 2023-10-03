e52c293f9ad588b3235162ff377b4e77b1cb1841
cenmelt <clement.collet123@orange.fr>
2023-10-03 14:01:28 +0200

    2eme pages

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PwdController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +18,18 @@ use App\Http\Controllers\PwdController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/form', function () {
     return view('form');
